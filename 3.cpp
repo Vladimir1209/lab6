ec4185743a26106dc7ec2eb6d60c029f99984a20
Vladimir1209 <108366469+Vladimir1209@users.noreply.github.com>
2024-11-16 16:20:54 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <set>
+#include <sstream>
 using namespace std;
-int wordsCounter(const string& str){
-    int s=1;
-    for(int i=0;i<str.size();++i){
-        if(str[i] == ' ' || str[i] == ','|| str[i] == '.'){
-            s++;
+int wordsCounter(const std::string& str) {
+    std::set<std::string> a;
+    std::stringstream ss(str);
+    std::string word;
+    while (ss >> word) {
+        while (!word.empty() && (word.back() == ' ' || word.back() == ',' || word.back() == '.')) {
+            word.pop_back();
+        }
+        while (!word.empty() && (word.front() == ' ' || word.front() == ',' || word.front() == '.')) {
+            word.erase(word.begin());
+        }
+        if (!word.empty()) {
+            a.insert(word);
         }
     }
-    return s;
-};
 
+    return a.size();
+}
 
-
-int main()
-{
-  int words = wordsCounter("can you can,can");
-  cout<<words;
+int main() {
+    int words = wordsCounter("can you can");
+    cout<<words;
 }
